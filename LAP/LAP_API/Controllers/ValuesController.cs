d805cf4c6cf36dc08f81725774912133c687489a
tkdfhr8744 <tkdfhr8744@naver.com>
2019-02-26 18:18:49 +0900

    프로젝트 컨트롤러 모듈

---
+++ LAP/LAP_API/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAP_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
